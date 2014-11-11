c7742f9a89d3d4370e4cabfdc36f0f9118504753
Brian Baskin <brian@thebaskins.com>
2014-11-11 08:20:08 -0500

    Initial file upload

---
+++ sndvol_acq.py
@@ -0,0 +1,111 @@
+﻿# Sound Mixer Acquisition from Live Registry
+# Retrieves a list of applications with sound stored by
+# Windows in the Sound Mixer preferences from a live system.
+# Version 1.0
+# @bbaskin - brian [at] thebaskins [dot] com
+# Released 11 Nov 14
+
+from __future__ import unicode_literals
+import datetime
+import sys
+try:
+    import _winreg
+except ImportError:
+    print 'This script must be run from within a Windows environment.'
+    quit()
+
+    
+def ConvertDate(ns):
+    """
+    Converts a given number of nanoseconds since 1 Jan 1601 to a standard date.
+    Arguments:
+        ns: number of nanoseconds
+    Results:
+        string based on a datetime structure
+    """
+    startDate = datetime.datetime(1601, 1, 1)
+    dateStruct = '%Y-%m-%d,%H:%M:%S'
+    dateModified = startDate + datetime.timedelta(seconds = ns*(10**-9)*100)
+    return dateModified.strftime(dateStruct)
+
+
+def GetOutputDevice(guid):
+    """
+    Acquires the Windows audio output device name for a given GUID
+    Arguments:
+        guid: Windows GUID to query
+    Results:
+        Device name or original GUID if no results
+    """
+    key = '%s\\%s\\%s' % (r'SOFTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Render', guid, 'Properties')
+
+    try:
+        deviceKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, key, 0, _winreg.KEY_READ | _winreg.KEY_WOW64_64KEY)
+    except WindowsError:
+        return guid
+    deviceNameKey = _winreg.QueryValueEx(deviceKey, '{a45c254e-df1c-4efd-8020-67d146a850e0},2')[0]
+    if deviceNameKey:
+        return deviceNameKey
+    else:
+        return guid
+
+
+def GetInputDevice(guid):
+    """
+    Acquires the Windows audio input device name for a given GUID
+    Arguments:
+        guid: Windows GUID to query
+    Results:
+        Device name or original GUID if no results
+    """
+    if guid == '{00000000-0000-0000-0000-000000000000}':
+        return 'N/A'
+
+    key = '%s\\%s\\%s' % (r'SOFTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Capture', guid, 'Properties')
+    try:
+        deviceKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, key, 0, _winreg.KEY_READ | _winreg.KEY_WOW64_64KEY)
+    except WindowsError:
+        return guid
+    deviceNameKey = _winreg.QueryValueEx(deviceKey, '{a45c254e-df1c-4efd-8020-67d146a850e0},2')[0]
+    if deviceNameKey:
+        return deviceNameKey
+    else:
+        return guid
+
+
+def main():
+    registryBase = r'SOFTWARE\Microsoft\Internet Explorer\LowRegistry\Audio\PolicyConfig\PropertyStore'
+    volumeSuffix = r'{219ED5A0-9CBF-4F3A-B927-37C9E5C5F14F}'
+    sndKey = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, registryBase)
+    sndKeyMeta = _winreg.QueryInfoKey(sndKey)
+    for i in range(0, sndKeyMeta[0]):
+        try:
+            storeKey = _winreg.EnumKey(sndKey,i)
+            sndAppKey = _winreg.OpenKey(sndKey,storeKey)
+            sndAppMeta = _winreg.QueryInfoKey(sndAppKey)
+            sndAppValue = _winreg.QueryValueEx(sndAppKey, "")
+        except EnvironmentError, WindowsError:
+            break 
+
+        try:
+            volumeRegistry = registryBase + '\\' + storeKey + '\\' + volumeSuffix
+            volumeKey = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, volumeRegistry)
+            volumeData = _winreg.QueryValueEx(volumeKey, '3')
+            volume = ord(volumeData[0][10])
+        except EnvironmentError, WindowsError:
+            volume = 'N/A'
+            
+        filename = unicode(sndAppValue[0]).split('|')[1].split('%')[0]
+        modDate = ConvertDate(int(sndAppMeta[2]))
+        OutputGUID = sndAppValue[0].split('|')[0].split('}.')[1]
+        OutputDevice = GetOutputDevice(OutputGUID)
+        InputGUID = sndAppValue[0].split("%b")[1]
+        InputDevice = GetInputDevice(InputGUID)
+        output = u'%s,%s,%s,%s,%s' % (modDate, OutputDevice, volume, InputDevice, filename)
+        print output.encode('utf8', 'replace')
+
+    _winreg.CloseKey(sndKey)
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ sndvol_acq_hive.py
@@ -0,0 +1,134 @@
+﻿# Sound Mixer Acquisition from Registry Hives
+# Retrieves a list of applications with sound stored by
+# Windows in the Sound Mixer preferences from a given set
+# of registry files.
+# Version 1.0
+# @bbaskin - brian [at] thebaskins [dot] com
+# Released 11 Nov 14
+
+from __future__ import unicode_literals
+import datetime
+import os
+import sys
+from argparse import ArgumentParser
+try:
+    from Registry import Registry
+except ImportError:
+    print 'This script requires python-registry, obtained from: http://www.williballenthin.com/registry/index.html'
+    quit()
+
+    
+def file_exists(fname):
+    """
+    Determine if a file exists
+    Arguments:
+        fname: path to a file
+    Results:
+        boolean value if file exists
+    """
+    return os.path.exists(fname) and os.access(fname, os.X_OK)
+
+    
+def GetOutputDevice(guid):
+    """
+    Acquires the Windows audio output device name for a given GUID
+    Arguments:
+        guid: Windows GUID to query
+    Results:
+        Device name or original GUID if no results
+    """
+    key = '%s\\%s\\%s' % (r'Microsoft\Windows\CurrentVersion\MMDevices\Audio\Render', guid, 'Properties')
+
+    HKLM = Registry.Registry('Reg\\SOFTWARE')
+    try:
+        deviceKey = HKLM.open(key)
+    except Registry.RegistryKeyNotFoundException:
+        return guid
+
+    try:
+        deviceNameKey = deviceKey.value('{a45c254e-df1c-4efd-8020-67d146a850e0},2').value()
+        return deviceNameKey
+    except Registry.RegistryValueNotFoundException:
+        return guid
+
+
+def GetInputDevice(guid):
+    """
+    Acquires the Windows audio input device name for a given GUID
+    Arguments:
+        guid: Windows GUID to query
+    Results:
+        Device name or original GUID if no results
+    """
+    if guid == '{00000000-0000-0000-0000-000000000000}':
+        return 'N/A'
+    key = '%s\\%s\\%s' % (r'Microsoft\Windows\CurrentVersion\MMDevices\Audio\Capture', guid, 'Properties')
+
+    HKLM = Registry.Registry('Reg\\SOFTWARE')
+    try:
+        deviceKey = HKLM.open(key)
+    except Registry.RegistryKeyNotFoundException:
+        return guid
+
+    try:
+        deviceNameKey = deviceKey.value('{a45c254e-df1c-4efd-8020-67d146a850e0},2').value()
+        return deviceNameKey
+    except Registry.RegistryValueNotFoundException:
+        return guid
+
+        
+def getArgs():
+    parser = ArgumentParser()
+    parser.add_argument('-s', '--software', help='Registry SOFTWARE file location', required=True)
+    parser.add_argument('-n', '--ntuser', help='Registry NTUSER.DAT file location', required=True)
+    args = parser.parse_args()
+    if not file_exists(args.software):
+        print "File not found: %s" % args.software
+        quit()
+    if not file_exists(args.ntuser):
+        print "File not found: %s" % args.ntuser
+        quit()
+    return args
+
+    
+def main():
+    args = getArgs()
+    registryBase = r'SOFTWARE\Microsoft\Internet Explorer\LowRegistry\Audio\PolicyConfig\PropertyStore'
+    volumeSuffix = '{219ED5A0-9CBF-4F3A-B927-37C9E5C5F14F}'
+    HKCU = Registry.Registry('Reg\\NTUSER.DAT')
+    
+    try:
+        sndKey = HKCU.open(registryBase)
+    except Registry.RegistryKeyNotFoundException:
+        print "Couldn't find Sound Mixer key. Exiting..."
+        quit()
+    
+    for storeKey in sndKey.subkeys():
+        try:
+            sndAppValue = storeKey.value("").value() # Get (Default) value
+            modDate = str(storeKey.timestamp()).replace(' ', ',').split('.')[0]
+        except EnvironmentError, WindowsError:
+            break 
+        except Registry.RegistryValueNotFoundException:
+            print "Unable to open key. This is an unexpected error."
+            quit()
+
+        volume = 0
+        try:
+            volumeKey = '%s\\%s\\%s' % (registryBase, storeKey.name(), volumeSuffix)
+            volumeData = HKCU.open(volumeKey).value('3').value()
+            volume = ord(volumeData[10])
+        except Registry.RegistryKeyNotFoundException:
+            volume = 'N/A'
+            
+        filename = unicode(sndAppValue).split('|')[1].split('%')[0]
+        OutputGUID = sndAppValue.split('|')[0].split('}.')[1]
+        OutputDevice = GetOutputDevice(OutputGUID)
+        InputGUID = sndAppValue.split("%b")[1]
+        InputDevice = GetInputDevice(InputGUID)
+        output = u'%s,%s,%s,%s,%s' % (modDate, OutputDevice, volume, InputDevice, filename)
+        print output.encode('utf8', 'replace')
+
+    
+if __name__ == '__main__':
+    main()
